e922b34f80efa64bbdecd899ee965d1fc0df0dc7
abramsonsari <abramsonsari@gmail.com>
2026-09-09 15:16:32 -0400

    Create Minutes to hours and minutes.py

---
+++ assignments/Minutes to hours and minutes.py
@@ -0,0 +1,16 @@
+'''
+""""""""""""""""""""""""""""""""""""""""""""""""
+This code asks the user for a number of minutes and calculates that into hours and minutes
+"""""""""""""""""""""""""""""""""""""""""""""""""
+'''
+
+'''
+This function will ask the user how many minutes they would like to calculate and will calculate that to hours and minutes and print the result.
+'''
+def minutes_to_hours_and_minutes():
+    how_many = int(input("How many minutes would you like to calculate?")) #getting a number input from the user
+    hours = how_many // 60 #converting the amount of minutes to how many hours using integer division
+    minutes = how_many % 60 #converting how many minutes there are
+    print(how_many, " minutes to hours and minutes is ", hours, " hours and ", minutes, " minutes!") #printing the result
+
+minutes_to_hours_and_minutes() #calling the function
